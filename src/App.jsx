c8f8ebfce5524b6a8921bec44a1949d026e90c40
Shahriar <shahriarh655@gmail.com>
2025-01-11 23:05:50 +0600

    Implemented the product list component, fetched all product data from the server, and displayed it on the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <ProductList />
     </div>
   );
 };
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,61 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+const retrieveProducts = async () => {
+  const response = await axios.get("http://localhost:3000/products");
+  return response?.data;
+};
+
+const ProductList = () => {
+  const {
+    isError,
+    isPending,
+    data: products,
+    error,
+  } = useQuery({
+    queryKey: ["products"],
+    queryFn: retrieveProducts,
+  });
+
+  if (isPending) {
+    return (
+      <div>
+        <p>Fetching Data...</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <p>Error:{error.message}</p>
+      </div>
+    );
+  }
+  return (
+    <div className="w-3/5">
+      <h1 className="text-3xl font-semibold text-black text-center py-8">
+        All Products
+      </h1>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4  gap-4">
+        {products &&
+          products.map((product) => (
+            <li className="border shadow-sm p-4 rounded-sm" key={product?.id}>
+              <img
+                src={product?.thumbnail}
+                alt="Product Image"
+                className="w-[300px] h-[300px] object-contain"
+              />
+              <h2 className="text-gray-800 font-medium line-clamp-1">
+                {product?.title}
+              </h2>
+              <button className="px-2 py-2 border border-cyan-500 font-medium text-sm text-cyan-500 rounded-xs w-full hover:bg-cyan-500 hover:text-white transition-all duration-300 rounded-sm mt-2">
+                See Details
+              </button>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
