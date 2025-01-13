d3c2895803b3f925fd086648aac66c4ccc33e607
Shahriar <shahriarh655@gmail.com>
2025-01-13 20:10:00 +0600

    Updated Field, FieldSet, and Login components. Applied some validation to the login form using React Hook Form.

--- src/components/Form/Field.jsx
+++ src/components/Form/Field.jsx
@@ -11,7 +11,7 @@ const Field = ({ label, children, htmlFor, error }) => {
 
   const id = htmlFor || getChildId(children);
   return (
-    <div className="flex flex-col items-start justify-start w-3/4 mx-auto">
+    <div className="flex flex-col items-start justify-start w-3/4">
       {label && <label htmlFor={id}>{label}</label>}
       {children}
       {!!error && (
--- src/components/Form/LoginForm.jsx
+++ src/components/Form/LoginForm.jsx
@@ -1,13 +1,39 @@
 import Field from "./Field";
 import FieldSet from "./FieldSet";
+import { useForm } from "react-hook-form";
 
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm();
+
+  const submitForm = (formData) => {
+    const user = {
+      email: "shahriar@gmail.com",
+      password: "12345678",
+    };
+
+    const found =
+      formData.email === user.email && formData.password === user.password;
+
+    if (!found) {
+      setError("root.random", {
+        message: `User With Email ${formData.email} Is Not Found`,
+        type: "random",
+      });
+    }
+    console.log(formData);
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(submitForm)}>
         <FieldSet label={"Enter Login Details"}>
-          <Field label={"Email"}>
+          <Field label={"Email"} error={errors.email}>
             <input
+              {...register("email", { required: "Email Is Required" })}
               type="email"
               name="email"
               id="email"
@@ -15,8 +41,19 @@ const LoginForm = () => {
               className="border-2 w-full px-3 py-2 rounded-sm text-black font-medium"
             />
           </Field>
-          <Field label={"Password"}>
+          <Field label={"Password"} error={errors.password}>
             <input
+              {...register("password", {
+                required: "Password Is Required",
+                minLength: {
+                  value: 8,
+                  message: "Password Must Be At Least Minimum 8 Characters.!",
+                },
+                maxLength: {
+                  value: 20,
+                  message: "Password Cannot Be More Than 20 Characters.!",
+                },
+              })}
               type="password"
               name="password"
               id="password"
@@ -24,7 +61,12 @@ const LoginForm = () => {
               className="border-2 w-full px-3 py-2 rounded-sm text-black font-medium"
             />
           </Field>
-
+          {/* error div */}
+          <div>
+            <span className="text-red-500">
+              {errors?.root?.random && errors?.root?.random?.message}
+            </span>
+          </div>
           <Field>
             <input
               type="submit"
